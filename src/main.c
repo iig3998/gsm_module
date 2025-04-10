32a26306a632a2ecd5e9cf8cd53efe73c6f0aa6f
Fabio Molon <fabiomolon85@gmail.com>
2025-04-10 12:36:21 +0200

    Cleans main file

--- src/main.c
+++ src/main.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-#include "FreeRTOS.h"
 #include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_cortex.h"
+#include "stm32f4xx_hal_def.h"
 
+#include "clock.h"
 
+/* Main program */
 int main(void) {
 
   HAL_Init();
 
+  HAL_NVIC_SystemReset();
+
   GPIO_InitTypeDef GPIO_InitStruct;
 
-  #if 0
-  GPIO_InitStruct.Pin = ;       // Specifies the GPIO pins to be configured.
-  GPIO_InitStruct.Mode = ;      // This parameter can be a value of @ref GPIO_mode_define */
-  GPIO_InitStruct.Pull = ;      // This parameter can be a value of @ref GPIO_pull_define */
-  GPIO_InitStruct.Speed = ;     // This parameter can be a value of @ref GPIO_speed_define */
-  GPIO_InitStruct.Alternate = ; //  Peripheral to be connected to the selected pins
-  #endif
   while (1) {
     HAL_GPIO_TogglePin (GPIOB, GPIO_PIN_12);
     HAL_Delay (100);

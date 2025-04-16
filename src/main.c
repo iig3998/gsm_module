110a644b689144a8cc98580ce5d8ef5ce681b820
Fabio Molon <fabiomolon85@gmail.com>
2025-04-16 14:40:25 +0200

    Test toggle LED on board stm32f411CEU6

--- src/main.c
+++ src/main.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
+#include <string.h>
+
 #include <stm32f4xx_hal.h>
 #include <stm32f4xx_hal_gpio.h>
 #include <stm32f4xx_hal_cortex.h>
 
 #include "clock.h"
+#include "gpio.h"
 
 /* Main program */
-int main(void) {
+int main() {
+
+  GPIO_InitTypeDef GPIO_InitStruct_PC13;
 
   HAL_Init();
 
   HAL_NVIC_SystemReset();
 
-  GPIO_InitTypeDef GPIO_InitStruct;
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  memset(&GPIO_InitStruct_PC13, 0, sizeof(GPIO_InitStruct_PC13));
+
+  /* COnfigure GPIO13 to drive LED on board */
+  GPIO_InitStruct_PC13.Pin = GPIO_PIN_13;
+  GPIO_InitStruct_PC13.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct_PC13.Pull = GPIO_NOPULL;
+  GPIO_InitStruct_PC13.Speed = GPIO_SPEED_FREQ_LOW;
+
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct_PC13);
 
   while (1) {
-    HAL_GPIO_TogglePin (GPIOB, GPIO_PIN_12);
+    HAL_GPIO_TogglePin (GPIOC, GPIO_PIN_13);
     HAL_Delay (100);
   }
 }
